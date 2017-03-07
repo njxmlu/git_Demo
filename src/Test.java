dadd96b96bebf3f540a9d17d833329e56acb03a3
njxmlu <wyemailaccplxm@163.com>
2017-03-07 11:26:34 +0800

    github commit test

---
+++ src/Test.java
@@ -0,0 +1,34 @@
+/*
+ * 江苏溯创信息科技有限公司
+ * 
+ * 项目名称 : GitHubProject
+ * 创建日期 : 2017年3月7日
+ * 修改历史 : 
+ *     1. [2017年3月7日]创建文件 by Administrator
+ */
+
+/**
+ * 添加类/接口功能描述
+ * @author Administrator
+ * 创建日期 : 2017年3月7日
+ */
+public class Test {
+
+    /**
+     * 
+     */
+    public Test() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * 添加方法功能描述
+     * @author Administrator
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("XXX");
+    }
+
+}
